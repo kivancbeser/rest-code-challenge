0201c95c3f76538c9527b860627efdcad823b74f
kivancbeser <beserkivanc@gmail.com>
2023-09-24 11:18:44 +0300

    Trying swagger implementation

--- src/main/java/com/practise/restcodechallenge/config/SwaggerConfig.java
+++ src/main/java/com/practise/restcodechallenge/config/SwaggerConfig.java
@@ -6,6 +6,7 @@ import io.swagger.annotations.Api;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -14,13 +15,18 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class SwaggerConfig {
 
     @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2).select().apis(withClassAnnotation(Api.class))
-            .paths(PathSelectors.any()).build();
+    public Docket api(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.practise."))
+                .paths(PathSelectors.any())
+                .build();
     }
 
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
     }
+
+
 }
